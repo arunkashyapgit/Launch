dd0e5ff8c6489ba2173f71c6532840bc030a1f58
arunkashyapgit <arunkashyaptestyantra@gmail.com>
2020-02-24 11:35:09 +0530

    second

--- LaunchBrwser/src/test/java/com/tests/LaunchBrowserTest.java
+++ LaunchBrwser/src/test/java/com/tests/LaunchBrowserTest.java
@@ -1,20 +1,21 @@
 package com.tests;
 
-import java.awt.AWTException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class LaunchBrowser {
+public class LaunchBrowserTest{
 	
-
-	public static void main(String[] args) throws InterruptedException, AWTException 
-	{
+@Test
+	public void Launch() {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http:www.google.com");
+		System.out.println(driver.getTitle());
+		
 
 }
 }
